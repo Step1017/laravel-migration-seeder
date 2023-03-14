cc2d8145e16fca0fb9ad94bdcde9f09ab43388d9
Step1017 <s.aveta@yahoo.com>
2023-03-14 16:41:36 +0100

    Aggiunto ciclo for per creare più treni

--- database/seeders/TrainsTableSeeder.php
+++ database/seeders/TrainsTableSeeder.php
@@ -20,27 +20,23 @@ class TrainsTableSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
-        //creo nuova istanza
-        $newTrain = new Train();
+        //se volessi creare 10 treni aggiungo ciclo for e rilancio il comando php artisan db:seed --class=TrainsTableSeeder nel terminale
+        for ($i = 0; $i < 10; $i++ ) {
+            $newTrain = new Train();
+            $newTrain->company = $faker->company();
+            $newTrain->departure_station = $faker->city();
+            $newTrain->arrival_station = $faker->city();
+                while ($newTrain->departure_station == $newTrain->arrival_station) {
+                    $newTrain->arrival_station = $faker->city();
+                }
+            $newTrain->departure_time = $faker->dateTimeBetween('-1 week', '+1 week');
+            $newTrain->arrival = $faker->dateTimeBetween('+1 week', '+2 weeks'); //per non sovrapporre arrival e departure time utilizzo + e +
+            $newTrain->code = strtoupper($faker->bothify('??##??'));
+            $newTrain->carragies = $faker->numberBetween(2, 15);
+            $newTrain->on_time = $faker->boolean();
+            $newTrain->canceled = $faker->boolean();
 
-        //aggiungo valori nelle varie colonne
-        $newTrain->company = $faker->company();
-        $newTrain->departure_station = $faker->city();
-        $newTrain->arrival_station = $faker->city();
-            //per evitare che departure e arrival siano uguali posso usare:
-            while ($newTrain->departure_station == $newTrain->arrival_station) {
-                $newTrain->arrival_station = $faker->city();
-            }
-        $newTrain->departure_time = $faker->dateTimeBetween('-1 week', '+1 week');
-        $newTrain->arrival = $faker->dateTimeBetween('+1 week', '+2 weeks'); //per non sovrapporre arrival e departure time utilizzo + e +
-        $newTrain->code = strtoupper($faker->bothify('??##??'));
-        $newTrain->carragies = $faker->numberBetween(2, 15);
-        $newTrain->on_time = $faker->boolean();
-        $newTrain->canceled = $faker->boolean();
-
-        //salvo nuova istanza
-        $newTrain->save();
+            $newTrain->save();
+        }
     }
 }
-
-//php artisan db:seed --class=TrainsTableSeeder => comando per eseguire il faker da terminale (crea e aggiunge un treno al db)
